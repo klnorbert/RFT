59a434534f9a5dea5bf73edc0dd808875d8ae0fe
wrath <wrath@192.168.0.108>
2020-11-29 16:09:12 +0100

    Hozzáadtam egy üdvözlést

--- Github/src/github/Github.java
+++ Github/src/github/Github.java
@@ -16,6 +16,7 @@ public class Github {
      */
     public static void main(String[] args) {
         System.out.println("Hello Ördög");
+        System.out.println("Hello Norbert!");
     }
     
 }
